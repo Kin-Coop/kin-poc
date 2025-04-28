032cf3f48080bb3a84675ab3db116b6cf8403d5d
ben mango <ben@benmango.co.uk>
2025-04-28 18:12:46 +0100

    adding ext ca.civicrm.contributionrecur

---
+++ web/sites/default/files/civicrm/ext/ca.civicrm.contributionrecur/CRM/Core/Payment/RecurOfflineACHEFT.php
@@ -0,0 +1,91 @@
+<?php
+/*
+ * Placeholder clas for offline recurring payments
+ */
+
+class CRM_Core_Payment_RecurOfflineACHEFT extends CRM_Core_Payment {
+
+  protected $_mode = NULL;
+
+  /**
+   * Constructor
+   *
+   * @param string $mode the mode of operation: live or test
+   *
+   * @return void
+   */
+  public function __construct($mode, &$paymentProcessor) {
+    $this->_mode = $mode;
+    $this->_paymentProcessor = $paymentProcessor;
+  }
+
+  /**
+   *
+   * @param  array $params assoc array of input parameters for this transaction
+   *
+   * @return array the result in a nice formatted array (or an error object)
+   */
+  public function doDirectPayment(&$params) {
+    if ($this->_mode == 'test') {
+      $query             = "SELECT MAX(trxn_id) FROM civicrm_contribution WHERE trxn_id LIKE 'test\\_%'";
+      $p                 = array();
+      $trxn_id           = strval(CRM_Core_Dao::singleValueQuery($query, $p));
+      $trxn_id           = str_replace('test_', '', $trxn_id);
+      $trxn_id           = intval($trxn_id) + 1;
+      $params['trxn_id'] = sprintf('test_%08d', $trxn_id);
+    }
+    else {
+      $query             = "SELECT MAX(trxn_id) FROM civicrm_contribution WHERE trxn_id LIKE 'live_%'";
+      $p                 = array();
+      $trxn_id           = strval(CRM_Core_Dao::singleValueQuery($query, $p));
+      $trxn_id           = str_replace('live_', '', $trxn_id);
+      $trxn_id           = intval($trxn_id) + 1;
+      $params['trxn_id'] = sprintf('live_%08d', $trxn_id);
+    }
+    $params['gross_amount'] = $params['amount'];
+    $params['payment_status_id'] = 1;
+    return $params;
+  }
+
+  public function getPaymentFormFields() {
+    return [];
+  }
+  
+  /**
+   * Are back office payments supported.
+   *
+   * @return bool
+   */
+  protected function supportsBackOffice() {
+    return TRUE;
+  }
+
+  /**
+   *
+   */
+  public function changeSubscriptionAmount(&$message = '', $params = array()) {
+    $userAlert = ts('You have updated the amount of this recurring contribution.');
+    CRM_Core_Session::setStatus($userAlert, ts('Warning'), 'alert');
+    return TRUE;
+  }
+
+  /**
+   *
+   */
+  public function cancelSubscription(&$message = '', $params = array()) {
+    $userAlert = ts('You have cancelled this recurring contribution.');
+    CRM_Core_Session::setStatus($userAlert, ts('Warning'), 'alert');
+    return TRUE;
+  }
+
+  /**
+   * This function checks to see if we have the right config values
+   *
+   * @return string the error message if any
+   */
+  public function checkConfig() {
+    return NULL;
+  }
+
+}
+
---
+++ web/sites/default/files/civicrm/ext/ca.civicrm.contributionrecur/templates/CRM/Contributionrecur/StartDate.tpl
@@ -0,0 +1,11 @@
+{*
+ Extra fields for recurring contribution page
+*}
+<div id="contributionrecur-extra">
+  <div class="crm-section start-date-section">
+    <div class="label">{$form.receive_date.label}</div>
+    <div class="content">{$form.receive_date.html}</div>
+    <div class="clear"></div>
+  </div>
+</div>
+
