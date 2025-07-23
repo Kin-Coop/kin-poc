b3ef323a3f151c7b1c50e6598235954582919a31
ben mango <ben@benmango.co.uk>
2025-07-23 12:58:41 +0100

    Forum updates and moving code to kin_forum

---
+++ web/modules/custom/kin_forum/src/Controller/DebugController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Drupal\kin_forum\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+class DebugController extends ControllerBase {
+  public function debugConstraints() {
+    //$field_definition = \Drupal::service('entity_field.manager')
+      //->getFieldDefinitions('node', 'group_forum')['field_group'];
+
+    //$constraints = $field_definition->getConstraints();
+
+    //dd($field_definition->getConstraints());
+
+    //$manager = \Drupal::service('plugin.manager.constraint');
+    //dd(array_keys($manager->getDefinitions()));
+
+    $violations = \Drupal::service('validator')->validate($node->field_group);
+    dd($violations);
+
+
+
+    //return [
+      //'#markup' => '<pre>' . print_r($constraints, TRUE) . '</pre>',
+    //];
+  }
+}
