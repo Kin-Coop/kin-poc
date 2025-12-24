f1a858c73ada424154629b2c90f4986e55a46abd
ben mango <ben@benmango.co.uk>
2025-12-24 14:47:15 +0000

    CivirulesActions

---
+++ web/sites/default/files/civicrm/ext/kincoop/CRM/CivirulesActions/CRM_CivirulesActions_Contribution_ContactEmail.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace CivirulesActions;
+
+use CRM_Civirules_TriggerData_TriggerData;
+
+class CRM_CivirulesActions_Contribution_ContactEmail extends \CRM_Civirules_Action
+{
+
+  /**
+   * @inheritDoc
+   */
+  public function processAction(CRM_Civirules_TriggerData_TriggerData $triggerData)
+  {
+    // TODO: Implement processAction() method.
+    $contactId = $triggerData->getContactId();
+    $entity = "Email";
+    $action = "send";
+    $params = array(
+      "template_id" => "83",
+      "from_name" => "Kin",
+      "from_email_option" => 1,
+      "contact_id = 70",
+      "from_email" => "admin@kin.coop",
+      "disable_smarty" => false,
+    );
+    civicrm_api3($entity, $action, $params);
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function getExtraDataInputUrl($ruleActionId)
+  {
+    // TODO: Implement getExtraDataInputUrl() method.
+    return FALSE;
+  }
+}
