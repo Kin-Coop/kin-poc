d6881bb5f55105ea009ba9591d1d2f3f6abb6642
ben mango <ben@benmango.co.uk>
2026-01-05 13:14:32 +0000

    Updating development site email redirect

--- web/sites/default/files/civicrm/ext/kincoop/kincoop.php
+++ web/sites/default/files/civicrm/ext/kincoop/kincoop.php
@@ -221,7 +221,7 @@ function kincoop_civicrm_postCommit($op, $objectName, $objectId, &$objectRef) {
 // Re-direct all emails to me on dev sites
 function kincoop_civicrm_alterMailParams(&$params, $context) {
 
-  if(str_contains( $_SERVER['HTTP_HOST'], 'dev')) {
+  if(str_contains( $_SERVER['HTTP_HOST'], 'dev.kin')) {
     $params['toEmail'] = 'ben@benmango.co.uk';
     $params['cc'] = 'ben@benmango.co.uk';
     $params['bcc'] = 'ben@benmango.co.uk';
