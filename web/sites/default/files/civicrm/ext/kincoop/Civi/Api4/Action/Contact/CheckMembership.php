aac82a623db6d550fd7b3668d9a20acd096d6790
ben mango <ben@benmango.co.uk>
2026-01-30 13:40:57 +0000

    Updates to check membership api4

--- web/sites/default/files/civicrm/ext/kincoop/Civi/Api4/Action/Contact/CheckMembership.php
+++ web/sites/default/files/civicrm/ext/kincoop/Civi/Api4/Action/Contact/CheckMembership.php
@@ -10,26 +10,23 @@ use Civi\Api4\Contact;
  * Contact.CheckMembership (APIv4)
  *
  * Checks all contacts tagged "Recurring_Member" (ID: 10).
- * If Membership_Valid_Until (custom_80) is today or earlier,
+ * If Membership_Valid_Until (custom_79) is today or earlier,
  * removes that tag and adds "Pending_Member" (ID: 8).
  * Skips trashed contacts (is_deleted = 1).
  */
 class CheckMembership extends AbstractAction {
 
-  /**
-   * Execute the job.
-   */
   public function _run(Result $result) {
     $today = (new \DateTime('today'))->format('Y-m-d');
 
     // 1️⃣ Find all qualifying contacts
-    $expiredContacts = Contact::get(FALSE)
+    $expiredContacts = Contact::get()
       ->addSelect('id')
-      ->addJoin('EntityTag AS et', 'INNER', 'id', 'et.entity_id')
-      ->addWhere('et.tag_id', '=', 10)
-      ->addWhere('et.entity_table', '=', 'civicrm_contact')
+      ->addJoin('EntityTag AS entity_tag', 'INNER', ['id', '=', 'entity_tag.entity_id'])
+      ->addWhere('entity_tag.tag_id', '=', 10)
+      ->addWhere('entity_tag.entity_table', '=', 'civicrm_contact')
       ->addWhere('is_deleted', '=', 0)
-      ->addWhere('custom_80', '<=', $today)
+      ->addWhere('Membership.Membership_Valid_Until', '<=', $today)
       ->execute();
 
     $ids = array_column(iterator_to_array($expiredContacts), 'id');
@@ -40,7 +37,7 @@ class CheckMembership extends AbstractAction {
     }
 
     // 2️⃣ Remove "Recurring_Member" tag (ID 10)
-    EntityTag::delete(FALSE)
+    EntityTag::delete()
       ->addWhere('entity_table', '=', 'civicrm_contact')
       ->addWhere('entity_id', 'IN', $ids)
       ->addWhere('tag_id', '=', 10)
@@ -48,7 +45,7 @@ class CheckMembership extends AbstractAction {
 
     // 3️⃣ Add "Pending_Member" tag (ID 8)
     foreach ($ids as $cid) {
-      EntityTag::create(FALSE)
+      EntityTag::create()
         ->addValue('entity_table', 'civicrm_contact')
         ->addValue('entity_id', $cid)
         ->addValue('tag_id', 8)
@@ -59,4 +56,3 @@ class CheckMembership extends AbstractAction {
     $result[] = ['message' => "Updated {$count} contacts (excluding trashed)."];
   }
 }
-
