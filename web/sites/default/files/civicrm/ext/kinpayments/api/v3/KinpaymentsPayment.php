3eb7e30124ae53e114ae182ed3cbd6431f69cf85
ben mango <ben@benmango.co.uk>
2026-06-25 09:50:55 +0100

    Kin payments update

--- web/sites/default/files/civicrm/ext/kinpayments/api/v3/KinpaymentsPayment.php
+++ web/sites/default/files/civicrm/ext/kinpayments/api/v3/KinpaymentsPayment.php
@@ -1,6 +1,11 @@
 <?php
 
-function _civicrm_api3_kinpayments_payment_get_spec(&$spec) {}
+function _civicrm_api3_kinpayments_payment_get_spec(&$spec) {
+  $spec['id'] = [
+    'title' => 'Payment ID',
+    'type' => CRM_Utils_Type::T_INT,
+  ];
+}
 
 function civicrm_api3_kinpayments_payment_get($params) {
 
--- web/sites/default/files/civicrm/ext/kinpayments/api/v3/KinpaymentsPayment/Get.spec.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function _civicrm_api3_kinpayments_payment_get_spec(&$spec) {
-  $spec['id'] = [
-    'title' => 'Payment ID',
-    'type' => CRM_Utils_Type::T_INT,
-  ];
-}
